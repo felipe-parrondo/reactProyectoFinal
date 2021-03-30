3a163aa6a7e7f697ece0229d2b293f5525b45a41
Felipe Parrondo Bastos <feliparrondobastos@gmail.com>
2021-03-29 21:42:28 -0300

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
+import 'react-bootstrap'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a>
-          Merceria Tefel
-        </a>
-      </header>
+      <header>
+            <div className="container-fluid d-flex justify-content-start">
+                <a href="index.html" id="header--logoCuadrado"> 
+                    <img className="img-fluid" src="multimedia/logos/main/logo_cuadrado.png"></img>
+                </a>
+
+                <a href="index.html" id="header--banner">
+                    Merceria Tefel
+                </a>
+            </div>
+
+            <nav className="container-fluid" id="menu">
+                <ul class="nav d-flex justify-content-around">
+                    <li className="nav-item menu__border" id="index--index"> <a href="index.html" className="nav-link text-white"> Inicio </a> </li>
+                    <li className="nav-item menu__border"> <a href="catalogo.html" className="nav-link text-white"> Cátalogo </a> </li>
+                    <li className="nav-item menu__border"> <a href="quienes-somos.html" className="nav-link text-white"> Quienes Somos </a> </li>
+                    <li className="nav-item menu__border"> <a href="preguntas-frecuentes.html" className="nav-link text-white"> Preguntas Frecuentes </a> </li>
+                    <li className="nav-item menu__border"> <a href="contacto.html" className="nav-link text-white"> Contacto </a> </li>
+                </ul>
+            </nav>
+        </header>
     </div>
   );
 }
