8e6206ba8d101dddb4feef4a9a11a734724f9c57
Felipe Parrondo Bastos <feliparrondobastos@gmail.com>
2021-04-26 18:53:35 -0300

    corte 26/04 a la tarde

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,24 +1,27 @@
 import "./ItemList.css"
 import {Link} from "react-router-dom";
 import ItemDetail from "../ItemDetail/ItemDetail.js"
-import axios from "axios"
-import React, {useEffect, useState} from "react"
+//import axios from "axios"
+import React from "react"
+import DatosObjs from "../../Data/Items/ItemListObjs.json"
 
 export default function ItemList () {
 
-    const [products, setProducts] = useState([])
+    /*const [products, setProducts] = useState([])
 
     useEffect( () => {
-        axios.get("https://docs.google.com/spreadsheets/d/1eo0FH500NO2WDdiTdXl_UhOEp3SxdnrhVQj-HJwtXws/edit#gid=0")
+        axios.get(rutaprueba)
         .then((res) =>{ 
             console.log(res)
             setProducts(res.data) 
         })
-    }, [])
+    }, [])*/                //Chrome no me permite acceder a localfiles de esta forma, asumo que hay que esperar a firebase
+
+    console.log(DatosObjs)
 
     return(
         <div className='catalogoItems--item'>
-            {products.map((i) =>{
+            {DatosObjs.map((i) =>{
                 return(
                     <div key={i.id} className="catalogoItems--innerItem" >
                         <img alt={i.name} src={i.pic}  /> 
